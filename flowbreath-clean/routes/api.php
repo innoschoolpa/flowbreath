2b5403f6da0dd551e0626e19bca17e63353a4d9f
박왕규 <innoschool@gmail.com>
2025-05-15 18:24:42 +0900

    테스트 파일 삭제

--- flowbreath-clean/routes/api.php
+++
@@ -1,6 +0,0 @@
-// SQL 리소스 관리 라우트
-$router->post('/resources/sql', 'ResourceController@uploadSQL');
-$router->get('/resources/sql', 'ResourceController@listSQL');
-$router->get('/resources/sql/:name', 'ResourceController@getSQL');
-$router->put('/resources/sql/:name', 'ResourceController@updateSQL');
-$router->delete('/resources/sql/:name', 'ResourceController@deleteSQL'); 
--- flowbreath-clean/src/Database/Seed.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace App\Database;
-
-abstract class Seed {
-    // ... existing code ...
-} 
