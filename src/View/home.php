263d968679bd2bff1cd401ec18ddcb7d023a656b
박왕규 <innoschool@gmail.com>
2025-05-21 20:47:57 +0900

    홈 /View/home.php content 180

--- src/View/home.php
+++ src/View/home.php
@@ -59,7 +59,7 @@ require_once __DIR__ . '/layouts/header.php';
                                 <i class="fa fa-user"></i> <?= htmlspecialchars($resource['username'] ?? $language->get('common.anonymous')) ?> ·
                                 <i class="fa fa-calendar"></i> <?= htmlspecialchars(substr($resource['created_at'],0,10)) ?>
                             </div>
-                            <p class="card-text mb-2"><?= htmlspecialchars(mb_strimwidth(strip_tags($resource['content']),0,80,'...')) ?></p>
+                            <p class="card-text mb-2"><?= htmlspecialchars(mb_strimwidth(strip_tags($resource['content']),0,180,'...')) ?></p>
                             <div class="mb-2">
                                 <?php foreach (($resource['tags'] ?? []) as $tag): ?>
                                     <span class="tag-badge">#<?= htmlspecialchars(is_array($tag) ? ($tag['name'] ?? '') : $tag) ?></span>
