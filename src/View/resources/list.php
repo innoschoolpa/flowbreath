54c3fe7ebfc631e216037be67e0fbf4ff8b113aa
박왕규 <innoschool@gmail.com>
2025-05-21 19:55:01 +0900

    리소스 컨텐트 200자로 조정

--- src/View/resources/list.php
+++ src/View/resources/list.php
@@ -98,7 +98,7 @@
                                 $preview = '';
                                 if (!empty($resource['content'])) {
                                     $plain = strip_tags($resource['content']);
-                                    $preview = mb_strimwidth($plain, 0, 300, '...');
+                                    $preview = mb_strimwidth($plain, 0, 200, '...');
                                 }
                                 echo htmlspecialchars($preview ?? '');
                                 ?>
