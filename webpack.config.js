7a40f769964c776f0521369375be7c86f3ba107f
박왕규 <innoschool@gmail.com>
2025-05-26 06:21:31 +0900

    속도 개선

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,60 @@
 // webpack.config.js
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
   entry: './public/assets/js/editor.js',
   output: {
-    filename: 'editor.bundle.js',
+    filename: '[name].[contenthash].js',
+    chunkFilename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'public/assets/js'),
+    clean: true,
   },
   mode: 'production',
+  resolve: {
+    extensions: ['.js'],
+    modules: [path.resolve(__dirname, 'node_modules')]
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true,
+          },
+        },
+      })
+    ],
+    splitChunks: {
+      chunks: 'all',
+      minSize: 20000,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+            return `vendor.${packageName.replace('@', '')}`;
+          },
+          priority: 20,
+        },
+        common: {
+          minChunks: 2,
+          priority: 10,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  },
+  performance: {
+    hints: 'warning',
+    maxEntrypointSize: 244000,
+    maxAssetSize: 244000,
+  },
   module: {
     rules: [
       {
@@ -17,9 +64,18 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
           },
         },
       },
     ],
   },
+  plugins: [
+    new CompressionPlugin({
+      test: /\.(js|css|html|svg)$/,
+      algorithm: 'gzip',
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
+  ],
 };
