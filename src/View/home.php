4660cde6e31569de44c3ab1655df720a8e68dbef
박왕규 <innoschool@gmail.com>
2025-05-27 04:45:10 +0900

    특수문자 제거12

--- src/View/home.php
+++ src/View/home.php
@@ -240,7 +240,7 @@ h1, h2, h3, h4, h5, h6 {
                         }
                         
                         // Determine content length based on YouTube link presence
-                        $contentLength = $hasYoutubeLink ? 130 : 300;
+                        $contentLength = $hasYoutubeLink ? 130 : 500;
                         
                         // Prepare content with only line breaks preserved
                         $content = strip_tags($resource['content'] ?? '');
