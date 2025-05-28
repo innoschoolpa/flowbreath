4fdf1104c522bce7f3b0e35a219c5e97065244b2
박왕규 <innoschool@gmail.com>
2025-05-28 19:24:00 +0900

    list.php 130:300

--- src/View/resources/list.php
+++ src/View/resources/list.php
@@ -278,7 +278,7 @@ input[type="text"]::placeholder {
       }
       
       // Determine content length based on YouTube link presence
-      $contentLength = $hasYoutubeLink ? 130 : 500; // Longer content for non-video resources
+      $contentLength = $hasYoutubeLink ? 130 : 300; // Longer content for non-video resources
       
       // Prepare content with only line breaks preserved
       $content = strip_tags($resource['content'] ?? '');
