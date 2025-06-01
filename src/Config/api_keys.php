532f839677495f7a94a611cd38397e18dcef62c4
박왕규 <innoschool@gmail.com>
2025-06-01 10:59:47 +0900

    /diary Tiny API key

--- src/Config/api_keys.php
+++ src/Config/api_keys.php
@@ -2,6 +2,6 @@
 
 return [
     'tinymce' => [
-        'api_key' => getenv('TINYMCE_API_KEY') ?: 'your-api-key-here'
+        'api_key' => getenv('TINYMCE_API_KEY') ?: '3p683d001w10l44tgvyk034uz5nsntitn1eiyjs24ufhx67a'
     ]
 ]; 
